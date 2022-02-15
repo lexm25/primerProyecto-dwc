3a5367075bf6ea86e4cbfa10d8d97e1a4c199bd6
lexm25 <alejandro.martin.alvarez.al@iespoligonosur.org>
2022-02-15 12:28:46 +0100

    clase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+export const routes: Routes = [
+{ path : 'empleado', component: EmpleadoComponent},
+{ path: 'fruta', component: AlumnadoDAWComponent},
+{ path: '', component: EmpleadoComponent},
+{ path: '**', component: EmpleadoComponent}
+];
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmpleadoComponent } from './empleado/empleado.component';
 import { AlumnadoDAWComponent } from './alumnado-daw/alumnado-daw.component';
+import { EMPTY } from 'rxjs';
+import { CocheComponent } from './coche/coche.component';
+import { HomeComponent } from './home/home.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmpleadoComponent,
-    AlumnadoDAWComponent
+    AlumnadoDAWComponent,
+    CocheComponent,
+    HomeComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
